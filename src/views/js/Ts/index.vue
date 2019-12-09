eb6b73d0d486a2af2e52d42318b5e486e23dc737
FengHaomiao <fenghaomiao@FengHaomiaodeMacBook-Pro.local>
2019-12-09 18:06:38 +0800

    feat: add ts

--- src/views/js/Ts/index.vue
+++ src/views/js/Ts/index.vue
@@ -5,6 +5,61 @@
     <h4>2.其它数据类型</h4>
     <p>1.any, unknown, never, 元祖类型, object类型, 枚举类型</p>
     <p>2.any与unknown的不同之处在于：虽然两者都可以指定任意数据类型，但是unknown被指定为某一个确定的数据类型之前，它不能被进行任何操作比如实例化、getter、函数执行等</p>
+    <p>3.接口interface</p>
+    <h4>3.函数重载</h4>
+    <h4>4.泛型</h4>
+    <pre class="code-content">
+function returnItem&lt;T&gt;(param: T): T {
+  return param
+}
+</pre>
+    <p>泛型接口</p>
+    <pre class="code-content">
+interface Direction&lt;T&gt; {
+  (para: T): T
+}
+</pre>
+    <p>泛型类</p>
+    <pre class="code-content">
+class Test&lt;T&gt; {
+  stack: Array&lt;T&gt; = []
+  public push(item: T) {
+    this.stack.push(item);
+  }
+  public pop() {
+    this.stack.pop()
+  }
+}
+let ff = new Test()
+ff.push('123')
+ff.push(1)
+console.log(ff.stack) // ['123', 1]
+ff.pop()
+console.log(ff.stack)
+</pre>
+    <p>可以使用&lt;TT extends xx&gt; 的方式来约束泛型</p>
+    <p>参数type的类型为{new(): T},就表示此泛型T是可被构造的，在被实例化后的类型是泛型T</p>
+    <pre class="code-content">
+function factory&lt;T&gt;(type: {new(): T}): T {
+  return new type()
+}
+</pre>
+    <h4>5.类型断言</h4>
+    <p>不建议使用双重类型断言</p>
+    <pre class="code-content">
+interface people {
+  name: string,
+  age: number,
+}
+const person = 'string' as any as people // 不推荐
+
+interface people {
+  name: string,
+  age: number,
+}
+const person = {} as people
+</pre>
+    <h4>6.类型解构</h4>
   </div>
 </template>
 <script>
